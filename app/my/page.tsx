ab787e9e7ec4527b5450b80e73e51e3696bb6b53
HyeonJi <guswly801@gmail.com>
2025-07-23 03:31:35 +0900

    내 정보 API 연결

--- app/my/page.tsx
+++ app/my/page.tsx
@@ -1,35 +1,63 @@
 "use client";
 
+import { getProfile } from "@/apis/user/getProfile";
+import logout from "@/apis/user/logout";
 import { COLORS } from "@/constant/color";
 import { Avatar, Divider, Stack, Typography } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import styled from "styled-components";
 
 export default function Home() {
   const router = useRouter();
+  const { data: userInfo } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  });
+
+  const onClickLogout = async () => {
+    try {
+      await logout();
+      if (confirm("로그아웃 되었습니다")) {
+        router.push("/siginin");
+      }
+    } catch (error) {
+      console.error("Logout failed:", error);
+      alert("로그아웃에 실패했습니다. 다시 시도해주세요.");
+    }
+  };
 
   return (
     <Container>
       <InfoContainer>
         <Avatar />
         <Stack flexDirection="column">
-          <Typography variant="body1">이름</Typography>
+          <Typography variant="body1">{userInfo?.username}</Typography>
           <Typography color={COLORS.GRAY_DARK} variant="body2">
-            이메일
+            {userInfo?.email}
           </Typography>
         </Stack>
       </InfoContainer>
       <Divider />
       <ButtonContainer
         onClick={() => {
+          location.href =
+            process.env.NEXT_PUBLIC_QNA_URL || window.location.origin;
+        }}
+      >
+        <Image src="/icons/setting.svg" alt="setting" width={24} height={24} />
+        <Typography>Q&A</Typography>
+      </ButtonContainer>
+      {/* <ButtonContainer
+        onClick={() => {
           router.push("/my/setting");
         }}
       >
         <Image src="/icons/setting.svg" alt="setting" width={24} height={24} />
         <Typography>설정</Typography>
-      </ButtonContainer>
-      <ButtonContainer>
+      </ButtonContainer> */}
+      <ButtonContainer onClick={onClickLogout}>
         <Image src="/icons/exit.svg" alt="logout" width={24} height={24} />
         <Typography>로그아웃</Typography>
       </ButtonContainer>
--- app/reward/page.tsx
+++ app/reward/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>리워드 페이지</div>;
+  return <div>리워드 페이지 (api 필요)</div>;
 }
