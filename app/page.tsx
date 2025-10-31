50dc5e1fce83b4cd151776bb9f78b83aa0a51a15
Hyeon Ji Kim <94117828+hyeon81@users.noreply.github.com>
2025-10-31 23:55:35 +0900

    fix: 메인 ui 수정

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,7 @@ import RewardMain from "@/components/main/RewardMain";
 
 export default function Home() {
   return (
-    <>
-      <div className="flex flex-col w-full h-full gap-6">
+      <div className="flex flex-col w-full h-full gap-4 pb-3">
         <RewardMain />
         <div className="grid grid-cols-2 gap-2">
           <BikeLogMain />
@@ -23,6 +22,5 @@ export default function Home() {
           <NewsMain />
         </div>
       </div>
-    </>
   );
 }
